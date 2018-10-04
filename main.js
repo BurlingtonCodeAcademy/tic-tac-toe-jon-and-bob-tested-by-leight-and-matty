a5797872d905bbc160239d2698e3498acb27985b
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-10-04 08:35:30 -0400

    merging

--- main.js
+++
@@ -1,8 +0,0 @@
-let letterX = '<img src=x.png" height="120" width="120">';
-let letterO = '<img src=o.png" height="120" width="120">';
-
-function winCheck() {}
-
-function play() {}
-
-function toggle() {}
