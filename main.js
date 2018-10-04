d92a27c14617d80b80ccbc746051237dc194727a
Habenzy <robertrstauss@gmail.com>
2018-10-04 12:06:17 -0500

    broken edit

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let playerTurn = document.getElementById('turn');
 let playerXMoves = [];
 let playerOMoves = [];
 let currentArray = playerXMoves;
+let numberOfMoves = 0
 //let activeCell = document.querySelectorAll('squares');
 //things to be implemented
 //target cell with on click event listener
@@ -17,12 +18,13 @@ for (i = 0; i < 9; i++) {
 }
 
 function winCheck() {
-  if (currentArray.includes('cell-0' && 'cell-1' && 'cell-2')) {
-    if (player === letterX) {
-      alert('Player X Wins!!!')
-    } else {alert('Player O Wins!!')}
+  while (numberOfMoves < 10) {
+    if (currentArray.includes('cell-0') && currentArray.includes('cell-1') && currentArray.includes('cell-2') || currentArray.includes('cell-3') && currentArray.includes('cell-4') && currentArray.includes('cell-5') || currentArray.includes('cell-6') && currentArray.includes('cell-7') && currentArray.includes('cell-8') || currentArray.includes('cell-0') && currentArray.includes('cell-3') && currentArray.includes('cell-6') || currentArray.includes('cell-1') && currentArray.includes('cell-4') &&currentArray.includes('cell-7') || currentArray.includes('cell-2') && currentArray.includes('cell-5') && currentArray.includes('cell-8') || currentArray.includes('cell-0') && currentArray.includes('cell-4') && currentArray.includes('cell-8') || currentArray.includes('cell-2') && currentArray.includes('cell-4') && currentArray.includes('cell-6')) {
+      alert(player + 'Wins!!')
   }
   //if winningMoves[[cell-0, cell-1, cell-2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]] player '?' wins, strike through winning slots
+  //algorithmicly check if adjacent squares include the same image then win; state machine?
+  //or add an if else that specifies player array
 }
 
 function play(e) {
@@ -30,8 +32,11 @@ function play(e) {
  e.currentTarget.innerHTML = player;
  currentArray.push(e.currentTarget.id);
  console.log(currentArray);
+ console.log(playerXMoves);
+ console.log(playerOMoves);
  winCheck();
- toggle();}
+ toggle();
+   }
 }
 //change player
 function toggle() {
