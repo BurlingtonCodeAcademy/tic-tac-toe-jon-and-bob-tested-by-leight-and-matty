ddae88c26d42d1c70e17f4b7bccbca1e3785bd35
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-10-04 16:21:54 -0400

    Working! Yay US!

--- main.js
+++ main.js
@@ -1,40 +1,66 @@
-let letterX = '<img src="x.png" height="120" width="120">';
-let letterO = '<img src="o.png" height="120" width="120">';
+let letterX = '<img src="x.png" height="120" width="120" id="x">';
+let letterO = '<img src="o.png" height="120" width="120" id="o">';
 let squares = document.getElementsByClassName('squares');
 let player = letterX
 let playerTurn = document.getElementById('turn');
 let playerXMoves = [];
 let playerOMoves = [];
 let currentArray = playerXMoves;
+let turnCount = 0;
 //let activeCell = document.querySelectorAll('squares');
-//things to be implemented
-//target cell with on click event listener
-//change player from x to o
-//set win conditions
-for (i = 0; i < 9; i++) {
-  square = document.getElementById(`cell-${i}`);
-  square.addEventListener('click', play);
+
+function clearBoard() {
+  square.innerHTML = '';
+  playerXMoves = [];
+  playerOMoves = [];
+  currentArray = playerXMoves;
+  playerTurn.innerHTML = "It is player X's turn";
+  turnCount = 0;
+}
+
+function start() {
+  for (i = 0; i < 9; i++) {
+    square = document.getElementById(`cell-${i}`);
+    clearBoard();
+    square.addEventListener('click', play);
+  }
+}
+
+start();
+
+function stop() {
+  for (i = 0; i < 9; i++) {
+    square = document.getElementById(`cell-${i}`);
+    square.removeEventListener('click', play);
+  }
 }
 
 function winCheck() {
-  console.log(currentArray);
-  if (currentArray.includes('cell-0') && currentArray.includes('cell-1') && currentArray.includes('cell-2') || currentArray.includes('cell-3') && currentArray.includes('cell-4') &&  currentArray.includes('cell-5') || currentArray.includes('cell-6') &&  currentArray.includes('cell-7') &&  currentArray.includes('cell-8') || currentArray.includes('cell-0') &&  currentArray.includes('cell-3') &&  currentArray.includes('cell-6') || currentArray.includes('cell-1') &&  currentArray.includes('cell-4') &&  currentArray.includes('cell-7') || currentArray.includes('cell-3') &&  currentArray.includes('cell-4') &&  currentArray.includes('cell-5') || currentArray.includes('cell-0') &&  currentArray.includes('cell-4') &&  currentArray.includes('cell-8') || currentArray.includes('cell-2') &&  currentArray.includes('cell-4') &&  currentArray.includes('cell-6')) {
+  if (currentArray.includes('cell-0') && currentArray.includes('cell-1') && currentArray.includes('cell-2') || currentArray.includes('cell-3') && currentArray.includes('cell-4') && currentArray.includes('cell-5') || currentArray.includes('cell-6') && currentArray.includes('cell-7') && currentArray.includes('cell-8') || currentArray.includes('cell-0') && currentArray.includes('cell-3') && currentArray.includes('cell-6') || currentArray.includes('cell-1') && currentArray.includes('cell-4') && currentArray.includes('cell-7') || currentArray.includes('cell-2') && currentArray.includes('cell-5') && currentArray.includes('cell-8') || currentArray.includes('cell-0') && currentArray.includes('cell-4') && currentArray.includes('cell-8') || currentArray.includes('cell-2') && currentArray.includes('cell-4') && currentArray.includes('cell-6')) {
     if (player === letterX) {
-      alert('Player X Wins!!!')
+      playerTurn.innerHTML = 'Player X Wins!!!';
+      stop();
     } else {
-      alert('Player O Wins!!')}
+      playerTurn.innerHTML = 'Player O Wins!!!';
+      stop();
+    }
+  } else if (turnCount === 9) {
+    playerTurn.innerHTML = "It's a DRAW.........";
+    stop();
+  } else {
+    toggle();
   }
-  //if winningMoves[[cell-0, cell-1, cell-2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]] player '?' wins, strike through winning slots
 }
 
 function play(e) {
   if (e.currentTarget.innerHTML === '') {
- e.currentTarget.innerHTML = player;
- currentArray.push(e.currentTarget.id);
- winCheck();
- toggle();}
+    e.currentTarget.innerHTML = player;
+    currentArray.push(e.currentTarget.id);
+    turnCount++;
+    winCheck();
+  }
 }
-//change player
+
 function toggle() {
   if (player === letterX) {
     player = letterO;
@@ -47,3 +73,4 @@ function toggle() {
     playerTurn.innerHTML = "It is player X's turn";
   };
 };
+
